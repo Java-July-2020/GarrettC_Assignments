b309a4b2a0fd5d1a8329a18b149993c43899a19b
LockStatic <31453209+LockStatic@users.noreply.github.com>
2020-07-09 15:15:02 -0700

    first Commit

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoremTest.java
@@ -0,0 +1,6 @@
+public class PythagoremTest {
+    public static void main (String args[]) {
+        PythagoreanTheorem legC = new PythagoreanTheorem();
+        System.out.println(legC.calculateHypotenuse(10, 20));
+    }
+}
